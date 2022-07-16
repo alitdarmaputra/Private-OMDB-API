e9048a7efba632dea5bc0d56f82124cb22fbe8e1
alitdarmaputra <alitdarmaputra@gmail.com>
2022-07-16 21:12:09 +0800

    Add pino middleware

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const pino = require("pino-http")();
 
 // Controller
 
 // Middleware
 app.use(bodyParser.json());
+app.use(pino);
 
 // Endpoint
 app.get("/", (req, res) => {
