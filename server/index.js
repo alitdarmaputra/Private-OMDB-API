8f6a8c2c0d889a85409c0d943019f5cd16ffd13f
alitdarmaputra <alitdarmaputra@gmail.com>
2022-07-16 22:46:25 +0800

    Add get movie url

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const app = express();
 const pino = require("pino-http")();
 
 // Controller
+const getMovieController = require("../controllers/getMovieController");
 
 // Middleware
 app.use(bodyParser.json());
 app.use(pino);
 
 // Endpoint
+app.get("/movies/:title", getMovieController);
 app.get("/", (req, res) => {
     res.send("Wellcome to OMDB Private API");
 });
